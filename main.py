bd6767c2f8f2dac5f094f0431101bdb7d3ea22dc
v.yamshchikov@velvetech.dev <74374952+Vasili-yamsh-hub@users.noreply.github.com>
2023-02-03 18:59:59 +0500

    added gui

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import re
-import flask
+import weather
+import customtkinter 
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+app = customtkinter.CTk()
+app.title("Программа Погода")
+
+app.mainloop()
+
