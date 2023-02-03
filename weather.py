031c765aeb9b97916c3b75ad6c65e20dad588f15
v.yamshchikov@velvetech.dev <74374952+Vasili-yamsh-hub@users.noreply.github.com>
2023-02-03 18:28:41 +0500

    added weather script

---
+++ weather.py
@@ -0,0 +1,41 @@
+import requests as re
+
+cityid = 0
+url_1 = "http://api.openweathermap.org/data/2.5/find"
+url_2 = "http://api.openweathermap.org/data/2.5/weather"
+city = "Verkhoyansk,RU"
+params = {
+    'q'    :city,
+    "type" : "like",
+    "lang" :"ru",
+    "appid": "8916ec839b0656f623079874ddb1afce",
+    "units": "metric"   
+    }  
+
+try: 
+    res = re.get(url_1, params)  
+    data = res.json()
+        
+    cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
+    #print("city:", cities)
+    cityid = data['list'][0]["id"]
+    #print("cityid = ", cityid)
+    
+    params_2 = {
+        'id'    :cityid,
+        "lang" :"ru",
+        "appid": "8916ec839b0656f623079874ddb1afce",
+        "units": "metric"   
+        }  
+        
+    res2 = re.get(url_2, params_2)
+    data2 = res2.json()
+    #print(cityid)
+    #print(data2)    
+    print("conditions:", data2['weather'][0]['description'])
+    print("temp:", data2['main']['temp'])
+    print("temp_min:", data2['main']['temp_min'])
+    print("temp_max:", data2['main']['temp_max'])
+    
+except:
+    pass
